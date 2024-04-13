ef59045477cd508cdbde2936a9d45c3ceade446f
RHMTshaikh <rahmatsheikh99@gmail.com>
2024-04-13 20:30:12 +0530

    http

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.status(200).json({mssg:'hello here'})
 })
 
