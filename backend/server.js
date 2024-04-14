d5e0fce41267e03a567a2cf7b2c1e6a8b6e560bf
RHMTshaikh <rahmatsheikh99@gmail.com>
2024-04-14 21:46:54 +0530

    npm install cors

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,19 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts.js')
 const userRoutes = require('./routes/user.js')
+const cors = require('cors')
 
 const app = express()
 
 // Enable CORS for all routes
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
+
+app.use(cors())
 
 //middleware
 app.use(express.json())
